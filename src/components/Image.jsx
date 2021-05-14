9152cfb866546c60c378342bd9e0a5d0039ac8a3
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-05-14 08:15:27 -0400

    import assests

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,7 +4,7 @@ import outstem_logo from "./images/outstem_logo_icon.png"
 const Image = () =>{
     return (
         <div>
-            <img src={backdrop} alt="back drop"/>
+            <img src={backdrop} className="backdrop" alt="back drop"/>
             <img src={outstem_logo} alt="outstem logo" Style="background: black"/>
         </div>
     )
